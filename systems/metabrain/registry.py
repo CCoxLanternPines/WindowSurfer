d0b97542318c5aece13a36a6793c8a3d2f66f5b2
krispyanimator <krispyanimator@gmail.com>
2025-08-23 12:03:35 -0500

    scrappin again.

--- systems/metabrain/registry.py
+++
@@ -1,9 +0,0 @@
-QUESTION_REGISTRY = {
-    "exh_edge_accuracy": ("exhaustion", "edge_accuracy"),
-    "flip_extrema_pct": ("reversal", "flip_extrema_pct"),
-    "inflect_leadtime": ("inflection", "leadtime_mean"),
-    "bottom_recovery_mag": ("bottom_catcher", "recovery_mag"),
-    "divergence_to_top": ("divergence", "div_to_top_pct"),
-    "peak_continuation": ("rolling_peak", "continuation_pct"),
-}
-
